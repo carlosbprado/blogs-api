a543dce90a01bfbb723f47c58a89db37c4322a91
carlosbprado <carlosbprado@outlook.com>
2023-01-26 16:26:37 -0300

    Requirement 2 done

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 const UserSchema = (sequelize, DataTypes) => {
  const UserTable = sequelize.define('User', {
-  display_name: DataTypes.STRING,
+  id: DataTypes.INTEGER,
+  displayName: DataTypes.STRING,
   email: DataTypes.STRING,
   password: DataTypes.STRING,
   image: DataTypes.STRING,
