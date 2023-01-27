2395bf623a316a6203afb5678083a59ea990dcd1
carlosbprado <carlosbprado@outlook.com>
2023-01-27 14:38:57 -0300

    Requisito 7 feito

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,6 +1,7 @@
 const CategorieSchema = (sequelize, DataTypes) => {
-  const CategorieTable = sequelize.define('categories', {
+  const CategorieTable = sequelize.define('Category', {
     name: DataTypes.STRING,
+    id: {type: DataTypes.INTEGER, primaryKey: true}
   }, {
     tableName: 'categories',
     underscored: true,
